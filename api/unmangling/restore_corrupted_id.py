f13edf660a14346a6facfec0bf02bf8d60fb7b69
Jeremy Craven <cjeremycraven@gmail.com>
2025-07-02 11:46:50 +0100

    further clarified logic in refine_outcomes

--- api/unmangling/restore_corrupted_id.py
+++ api/unmangling/restore_corrupted_id.py
@@ -226,18 +226,23 @@ def refine_outcome_codes(
             if (analysis.r_cid is not None) ^ (analysis.r_did is not None):
                 analysis.outcome_code = OutcomeCodes.POSSIBLE_CORRUPTION_UNAMBIG
 
-                if (analysis.r_cid == analysis.sctid_provided) or (
-                    analysis.r_did == analysis.sctid_provided # this case can only happen for 16 digits
-                ):
+                if analysis.sctid_provided in [analysis.r_cid, analysis.r_did]: #see comment below
                     analysis.outcome_code = OutcomeCodes.ANY_CORRUPTION_IS_SILENT
 
-            elif (analysis.r_cid is not None) and (analysis.r_did is not None):
+            elif (analysis.r_cid is not None) and (analysis.r_did is not None): #see comment below
                 analysis.outcome_code = OutcomeCodes.POSSIBLE_CORRUPTION_AMBIG
 
-                if analysis.r_cid == analysis.sctid_provided: # no need to check for r_did because can only happen for 16 digits
-                                                              # and 16 digits can never be ambiguous
+                if analysis.sctid_provided in [analysis.r_cid, analysis.r_did]:
                     analysis.outcome_code = OutcomeCodes.AMBIG_COULD_BE_SILENT
-
+               
             else:  # i.e. (analysis.r_cid is None) and (analysis.r_did is None):
                 assert (analysis.r_cid is None) and (analysis.r_did is None)
                 analysis.outcome_code = OutcomeCodes.NO_RECONSTRUCTIONS_EXIST
+
+            # re the tests for "SILENT"
+            # these are both written the same for clarity.
+            # however the provided_sctid==r_did case can only happen for 16 digits and it can never be ambiguous so only 
+            # really needed for the unambiguous ANY_CORRUPTION_IS_SILENT case
+            # and the did case does not really need to be considered for the ambiguous case, as ambiguity
+            # can only occur for 17 or 18 digits and an entered sctid can never look like a did in that case
+            # I think coded like this the intent is clear even if certain of the conditions can never be true
